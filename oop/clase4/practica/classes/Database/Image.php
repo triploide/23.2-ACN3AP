0d38cadecfae0dd7f83966097b58b00d496ef8cf
Maxi Yañez <maxiyanez84@gmail.com>
2023-09-06 15:25:28 -0300

    OOP - Clase 4 - Setup

---
+++ oop/clase4/practica/classes/Database/Image.php
@@ -0,0 +1,44 @@
+<?php
+
+class Image
+{
+    private $pdo;
+    private $data;
+
+    public function __construct()
+    {
+        try {
+            $this->pdo = new PDO('mysql:dbname=test;chartset=UTF-8;host=localhost', 'root', '');
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public static function find(int $id) : Image
+    {
+        $image = new Image;
+
+        $stmt = $image->pdo->prepare('SELECT * FROM images WHERE id = :id');
+        $stmt->execute(['id' => $id]);
+        $response = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        $image->setData($response);
+
+        return $image;
+    }
+
+    public function getId() : int
+    {
+        return (int)$this->data['id'];
+    }
+
+    public function getSrc() : string
+    {
+        return $this->data['src'];
+    }
+
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+}
---
+++ oop/clase4/practica/classes/Intervention/Image.php
@@ -0,0 +1,35 @@
+<?php
+
+class Image
+{
+    private $resource;
+
+    public function __construct($path)
+    {
+        $this->resource = imagecreatefromjpeg($path);
+    }
+
+    public function crop($width, $height)
+    {
+        $width = min($width, imagesx($this->resource));
+        $height = min($height, imagesy($this->resource));
+
+        $centerX = round(imagesx($this->resource) / 2);
+        $centerY = round(imagesy($this->resource) / 2);
+
+        $widthHalf  = round($width / 2);
+        $heightHalf = round($height / 2);
+
+        $x = max(0, $centerX - $widthHalf);
+        $y = max(0, $centerY - $heightHalf);
+
+        $this->resource = imagecrop($this->resource, ['x' => $x, 'y' => $y, 'width' => $width, 'height' => $height]);
+    }
+
+    public function save($path, $quality = 75)
+    {
+        imagejpeg($this->resource, $path, $quality);
+
+        imagedestroy($this->resource);
+    }
+}
